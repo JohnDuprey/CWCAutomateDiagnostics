0ee71371788d71192e44f0f857c3fd1d933655cd
John Duprey <jwd@johnwduprey.com>
2019-02-18 16:39:52 -0500

    cleanup of comments, added start-service after update to ensure services are restarted properly

--- AutomateDiagnostics.ps1
+++ AutomateDiagnostics.ps1
@@ -27,40 +27,6 @@ function extractHostname($url) {
 
 # Author: Joakim Borger Svendsen, 2017. http://www.json.org
 # Svendsen Tech. Public domain licensed code.
-# v0.3, 2017-04-12 (second release of the day, I actually read some JSON syntax this time)
-#       Fixed so you don't double-whack the allowed escapes from the diagram, not quoting null, false and true as values.
-# v0.4. Scientific numbers are supported (not quoted as values). 2017-04-12.
-# v0.5. Adding switch parameter EscapeAllowedEscapesToo (couldn't think of anything clearer),
-#       which also double-whacks (escapes with backslash) allowed escape sequences like \r, \n, \f, \b, etc.
-#       Still 2017-04-12.
-# v0.6: It's after midnight, so 2017-04-13 now. Added -QuoteValueTypes that makes it quote null, true and false as values.
-# v0.7: Changed parameter name from EscapeAllowedEscapesToo to EscapeAll (... seems obvious now). Best to do it before it's
-#       too late. 2017-04-13.
-# v0.7.1: Made the +/- after "e" in numbers optional as this is apparently valid (as plus, then)
-# v0.8: Added a -Compress parameter! 2017-04-13.
-# v0.8.1: Fixed bug that made "x.y" be quoted (but scientific numbers and integers worked all the while). 2017-04-14.
-# v0.8.2: Fixed bug with calculated properties (yay, this improves flexibility significantly). 2017-04-14.
-# v0.9: Almost too many changes to mention. Now null, true and false as _value types_ are unquoted, otherwise they
-#       are quoted. Comparing to the PowerShell team's ConvertTo-Json. Now escaping works better and more
-#       standards-conforming. If you have a newline in the strings, it'll be replaced by "\n" (literally, not a newline),
-# while if you have "\n" literally, it'll turn into \\n. Code quality improvements. Refactoring. Still some more to fix,
-# but it's getting better. Datetime stuff is bothering me, not sure I like how it's handled in the PS team's cmdlet, but I
-# don't have a sufficiently informed opinion.
-#
-# v0.9.1: Formatting fixes.
-# v0.9.2: Returning proper value types when sending in only single values of $true and $false (passed through).
-#         $null is buggy, but only if you pass in _nothing_ else, but $null. As a value in an array, hash or
-#         anywhere else, it works fine.
-# v0.9.2.1: Forgot.
-# v0.9.2.2: Adding escaping of "solidus" (forward slash).
-# v0.9.3: Coerce numbers from strings only if -CoerceNumberStrings is specified (non-default), properly detect numerical types and
-#         by default omit double quotes only on these.
-# v0.9.3.1: Respect and do not doublewhack/escape (regex) "\u[0-9a-f]{4}".
-# v0.9.3.2: Undoing previous change ... (wrong logic).
-# v0.9.3.3: Comparing to the PS team's ConvertTo-Json again and they don't escape "/" alone. Undoing 0.9.2.2 change.
-# v0.9.3.4: Support the IA64 platform and int64 on that too.
-# v0.9.4.0: Fix nested array bracket alignment issues. 2017-10-21.
-######################################################################################################
 
 # Take care of special characters in JSON (see json.org), such as newlines, backslashes
 # carriage returns and tabs.
@@ -345,7 +311,9 @@ Function Start-AutomateDiagnostics {
         taskkill /im ltsvcmon.exe /f
         taskkill /im lttray.exe /f
         $results = Update-LTService -WarningVariable updatetest -WarningAction SilentlyContinue
-        Start-Sleep -Seconds 30
+        Start-Sleep -Seconds 45
+        Start-Service LTService
+        Start-Service LTSvcMon
 		$info = Get-LTServiceInfo
 		if ([version]$info.Version -gt [version]$current_version) {
 			$update_text = 'Updated from {1} to {0}' -f $info.Version,$current_version
