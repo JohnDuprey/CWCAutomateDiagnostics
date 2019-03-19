c68b8139f895d78f6bf5832d3bce9fd155aedf1c
John Duprey <jwd@johnwduprey.com>
2019-03-19 17:31:34 -0400

    Update to powershell - added server failure reasons to status message

--- AutomateDiagnostics.ps1
+++ AutomateDiagnostics.ps1
@@ -383,15 +383,15 @@ Function Start-AutomateDiagnostics {
                 $server_msg = "No automate servers detected"
             }
             elseif ($server_test -eq $false -and $servers.Count -gt 0) {
-                $server_msg = "Error running Automate server tests"
+                $server_msg = "Error"
                 if (!($compare_test)) {
-                    $server_msg = "$server_msg | Server address not matched"
+                    $server_msg = $server_msg + " | Server address not matched"
                 }
                 if (!($conn_test)) {
-                    $server_msg = "$server_msg | Ping failure"
+                    $server_msg = $server_msg + " | Ping failure"
                 }
                 if ($target_version -eq "") {
-                    $server_msg = "$server_msg | Version check fail"
+                    $server_msg = $server_msg + " | Version check fail"
                 }
             }
 
