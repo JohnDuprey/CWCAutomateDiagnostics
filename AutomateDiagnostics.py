4fbc9e816ce3bdcf2bff2df49368b6d6414063e9
John Duprey <jduprey@cnsit.com>
2020-10-11 20:02:53 -0400

    Added agent.log file support to python Added update support to python script

--- AutomateDiagnostics.py
+++ AutomateDiagnostics.py
@@ -2,18 +2,49 @@ import json
 import os
 import subprocess
 import platform
+import base64
+import getopt
+import sys
 
 def system_call(command):
-    p = subprocess.Popen([command], stdout=subprocess.PIPE, shell=True)
-    return p.stdout.read()
+	p = subprocess.Popen([command], stdout=subprocess.PIPE, shell=True)
+	return p.stdout.read()
 
 # Read state file for info
 with open("/usr/local/ltechagent/state","r") as read_file:
 	data = json.load(read_file)
 
+lterrors = ""
+try:
+	opts, args = getopt.getopt(sys.argv[1:],"e")
+	for opt, arg in opts:
+		if opt == '-e':
+			with open("/usr/local/ltechagent/agent.log","r") as log_file:
+				lterrors = log_file.read()
+except getopt.GetoptError:
+	pass
+
+
+lterrors_bytes = lterrors.encode("ascii")
+lterrors_b64_bytes = base64.b64encode(lterrors_bytes)
+lterrors_str = lterrors_b64_bytes.decode("ascii")
+
 # Get last contact date
 lc = data["last_contact"]
 last_contact = "{0}/{1}/{2} {3}:{4:02d}:{5:02d}".format(lc["month"],lc["day_of_month"],lc["year"],lc["hour"],lc["min"],lc["sec"])
+
+old_version = data["version"]
+
+system_call("/usr/local/ltechagent/ltupdate")
+
+# Read state file for info
+with open("/usr/local/ltechagent/state","r") as read_file:
+	data = json.load(read_file)
+
+if old_version != data["version"]:
+	update = "Updated from "+old_version+" to "+ data["version"]
+else:
+	update = "Already updated to "+ data["version"]
 
 # Check services
 if platform.system() == 'Darwin':
@@ -46,11 +77,12 @@ elif platform.system() == 'Linux':
 diag_result = { 
 	'server_addr': data["last_good_server_url"], 
 	'lastcontact': last_contact,
-	'update': data["version"],
+	'update': update,
 	'version': data["version"],
 	'id': data['computer_id'],
 	'online': data["is_signed_in"],
-	'svc_ltservice': svc_ltsvc
+	'svc_ltservice': svc_ltsvc,
+	'lterrors': lterrors_str
 }
 
 print("!---BEGIN JSON---!")
--- Initializer.js
+++ Initializer.js
@@ -57,6 +57,8 @@ SC.event.addGlobalHandler(SC.event.InitializeTab, function (eventArgs) {
 		SC.ui.addElement(eventArgs.container, 'DIV', {id: 'lastUpdateContainer'});
 		SC.ui.addElement(eventArgs.container, 'DIV', {id: 'dataContainer'});
 		SC.ui.addElement(eventArgs.container, 'TABLE', {id: 'dataTable'});
+		SC.ui.addElement(eventArgs.container, 'DIV', {id: 'repairOptions'});
+		SC.ui.addElement(eventArgs.container, 'DIV', {id: 'lterrors'});
 	}
 });
 
@@ -336,13 +338,19 @@ function displayDataJson(json) {
 	SC.ui.addElement($('dataTable'), 'tr', {id: 'locationid_row'});
 	SC.ui.addElement($('locationid_row'), 'th', {id: 'locationid_hdr', innerHTML: 'Location ID'});
 	SC.ui.addElement($('locationid_row'), 'td', {id: 'locationid', innerHTML:json["locationid"], colspan: 2});
-	SC.ui.addElement($('dataTable'), 'tr', {id: 'repair_row1'});
-	SC.ui.addElement($('dataTable'), 'tr', {id: 'repair_row2'});
-	SC.ui.addElement($('repair_row1'), 'th', {id: 'repair_hdr', innerHTML: 'Repair Option'});
-	SC.ui.addElement($('repair_row2'), 'th', {id: 'repair_hdr', innerHTML: 'Repair Option'});
-	var repairCol1 = SC.ui.addElement($('repair_row1'), 'td', {id: 'repair', innerHTML:"", colspan: 2});
-	var repairCol2 = SC.ui.addElement($('repair_row2'), 'td', {id: 'repair', innerHTML:"", colspan: 2});
+	//SC.ui.addElement($('dataTable'), 'tr', {id: 'repair_row1'});
+	//SC.ui.addElement($('dataTable'), 'tr', {id: 'repair_row2'});
+	SC.ui.addElement($('repairOptions'),'h3',{id: 'repair_hdr', innerHTML: "Repair Options"});
+	var repairCol1 = SC.ui.addElement($('repairOptions'), 'div', {id: 'restartOption'});
+	var repairCol2 = SC.ui.addElement($('repairOptions'), 'div', {id: 'reinstallOption'});
+	//var repairCol1 = SC.ui.addElement($('restartOption'), 'td', {id: 'repair', innerHTML:"", colspan: 2});
+	//var repairCol2 = SC.ui.addElement($('reinstallOPtion'), 'td', {id: 'repair', innerHTML:"", colspan: 2});
 	//var repairButton = {commandName: json["repair"], commandArgument: 'Automate', text: json["repair"]};
+
+	if (json['lterrors'] != "") {
+		SC.ui.addElement($('lterrors'),'h3',{id: 'lterrors_hdr', innerHTML: "LTErrors.txt"});
+		SC.ui.addElement($('lterrors'),'pre',{id: 'lterrors_file', innerHTML: atob(json['lterrors'])});
+	}
 
 	SC.command.queryAndAddCommandButtons(repairCol1, 'RestartButton');
 	SC.command.queryAndAddCommandButtons(repairCol2, 'ReinstallButton');
@@ -393,7 +401,7 @@ function getAutomateCommandText(headers) {
 			if (isNaN(txtlocationid)) { txtlocationid = "1"; }
 			return "$WarningPreference='SilentlyContinue'; IF([Net.SecurityProtocolType]::Tls) {[Net.ServicePointManager]::SecurityProtocol=[Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls}; IF([Net.SecurityProtocolType]::Tls11) {[Net.ServicePointManager]::SecurityProtocol=[Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls11}; IF([Net.SecurityProtocolType]::Tls12) {[Net.ServicePointManager]::SecurityProtocol=[Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12};(new-object Net.WebClient).DownloadString('"+getLTPoSh()+"') | iex; Reinstall-LTService -SkipDotNet -Server https://"+getLTServer()+" -LocationID "+txtlocationid + " -InstallerToken " + txtinstallertoken;
 			return 
-		case "sh/bash/json/Automate": return "url="+getLinuxDiagnosticsURL()+"; CURL=$(command -v curl); WGET=$(command -v wget); if [ ! -z $CURL ]; then echo $($CURL -s $url | python); else echo $($WGET -q -O - --no-check-certificate $url | python); fi"; 
+		case "sh/bash/json/Automate": return "url="+getLinuxDiagnosticsURL()+"; CURL=$(command -v curl); WGET=$(command -v wget); if [ ! -z $CURL ]; then echo $($CURL -s $url | python); else echo $($WGET -q -O - --no-check-certificate $url | python - -e); fi"; 
     	default: throw "unknown os";
 	}
 }
