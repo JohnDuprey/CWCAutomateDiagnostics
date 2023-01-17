24772fba6081930b4348f015615292502311f08b
Joe McCall <joe.mccall@executech.com>
2023-01-16 19:41:44 -0800

    - Incorporating ASYNC changes from DEV channel - Fixed event data parsing - Split Linux and Mac diagnostic to resolve python compatibility issues - Incorporated the LTErrors output - CSS changes for readability

--- SessionEventTrigger.cs
+++ SessionEventTrigger.cs
@@ -11,57 +11,56 @@ using System.Text.RegularExpressions;
 using System.Reflection;
 public class SessionEventTriggerAccessor : IDynamicSessionEventTrigger
 {
+	// 2023.01.16 -- Joe McCall | Imported the newer ASync method from DEV branch
 	public Proc GetDeferredActionIfApplicable(SessionEventTriggerEvent sessionEventTriggerEvent)
 	{
-		var maintenance = ExtensionContext.Current.GetSettingValue("MaintenanceMode");
-		var usemachinename = ExtensionContext.Current.GetSettingValue("SetUseMachineName");
-		var agentidproperty = Int32.Parse(ExtensionContext.Current.GetSettingValue("AgentIDCustomProperty"));
-		var agentversionproperty = Int32.Parse(ExtensionContext.Current.GetSettingValue("AgentVersionCustomProperty"));
-		
-		if (sessionEventTriggerEvent.SessionEvent.EventType == SessionEventType.Connected && 
-			sessionEventTriggerEvent.SessionConnection.ProcessType == ProcessType.Guest && maintenance == "0" && sessionEventTriggerEvent.Session.ActiveConnections.Where(_ => _.ProcessType == ProcessType.Host).Count() == 0) {
-			return (Proc)delegate {	RunDiagnostics(sessionEventTriggerEvent); };
+		if (
+			sessionEventTriggerEvent.SessionEvent.EventType == SessionEventType.Connected
+			&& sessionEventTriggerEvent.SessionConnection.ProcessType == ProcessType.Guest
+			&& ExtensionContext.Current.GetSettingValue("MaintenanceMode") == "0"
+			&& sessionEventTriggerEvent.Session.ActiveConnections.Where(_ => _.ProcessType == ProcessType.Host).Count() == 0
+		) {
+			return () => ScreenConnect.TaskExtensions.InvokeSync(async delegate {
+				 RunDiagnostics(sessionEventTriggerEvent, ExtensionContext.Current);
+			});
 		}
-		else if (sessionEventTriggerEvent.SessionEvent.EventType == SessionEventType.RanCommand) {
-			return (Proc)delegate
-				{
-					var sessionDetails = SessionManagerPool.Demux.GetSessionDetails(sessionEventTriggerEvent.Session.SessionID);
-                    string output = sessionEventTriggerEvent.SessionEvent.Data;
+		else if (
+			sessionEventTriggerEvent.SessionEvent.EventType == SessionEventType.RanCommand
+			&& IsDiagnosticContent(sessionEventTriggerEvent.SessionEvent.Data)
+		) {
+			return () => ScreenConnect.TaskExtensions.InvokeSync(async delegate
+			{
+				try
+					{
+						var sessionDetails = SessionManagerPool.Demux.GetSessionDetailsAsync(sessionEventTriggerEvent.Session.SessionID);
+	                    string output = sessionEventTriggerEvent.SessionEvent.Data;
 
-					try {
-						if (IsDiagnosticContent(output) && IsDiagResult(output)) {
+						if (IsDiagResult(output)) {
 							var data = output.Split(new string[] { "!---BEGIN JSON---!" }, StringSplitOptions.None);
 							if (data[1] != "") {
-								//WriteLog(data[1]);
-								string pattern = @"(\{(.|\s)*\})";
-								Match m = Regex.Match(data[1],pattern);
-								if (m.Success) {
-									string json = m.Groups[1].Value;
-									//WriteLog(json);
-									DiagOutput diag = Deserialize(json);
-									var session = sessionEventTriggerEvent.Session;
-									if (diag.version != null) {
-										session.CustomPropertyValues[agentversionproperty - 1] = diag.version;
-									}
-									if (diag.id != null) {
-										session.CustomPropertyValues[agentidproperty - 1] = diag.id;
-									}
-									var sessionname = session.Name;
-									if (usemachinename == "1") {
-										sessionname = "";
-									}
-									SessionManagerPool.Demux.UpdateSession("AutomateDiagnostics", session.SessionID, sessionname, session.IsPublic, session.Code, session.CustomPropertyValues);
+								DiagOutput diag = Deserialize(data[1]);
+								var session = sessionEventTriggerEvent.Session;
+								if (diag.version != null) {
+									session.CustomPropertyValues[Int32.Parse(ExtensionContext.Current.GetSettingValue("AgentVersionCustomProperty")) - 1] = diag.version;
 								}
+								if (diag.id != null) {
+									session.CustomPropertyValues[Int32.Parse(ExtensionContext.Current.GetSettingValue("AgentIDCustomProperty")) - 1] = diag.id;
+								}
+								var sessionname = session.Name;
+								if (ExtensionContext.Current.GetSettingValue("SetUseMachineName") == "1") {
+									sessionname = "";
+								}
+								await SessionManagerPool.Demux.UpdateSessionAsync("AutomateDiagnostics", session.SessionID, sessionname, session.IsPublic, session.Code, session.CustomPropertyValues);
 							}
 						}
-						else if (IsDiagnosticContent(output) && IsRepairResult(output)) {
-							RunDiagnostics(sessionEventTriggerEvent);
+						else if (IsRepairResult(output)) {
+							RunDiagnostics(sessionEventTriggerEvent, ExtensionContext.Current);
 						}
 					}
 					catch (Exception e) {
 						WriteLog(e.Message);
 					}
-				};
+			});
 		}
 		return null;
 	}
@@ -72,36 +71,43 @@ public class SessionEventTriggerAccessor : IDynamicSessionEventTrigger
         	return ser.ReadObject(ms) as DiagOutput;
         }
     }
-	private void RunDiagnostics(SessionEventTriggerEvent sessionEventTriggerEvent) {
-		var sessionDetails = SessionManagerPool.Demux.GetSessionDetails(sessionEventTriggerEvent.Session.SessionID);
+
+	// 2023.01.16 -- Joe McCall | Imported the newer ASync method from DEV branch
+	private async void RunDiagnostics(SessionEventTriggerEvent sessionEventTriggerEvent, ExtensionContext extensionContext) {
+		var sessionDetails = await SessionManagerPool.Demux.GetSessionDetailsAsync(sessionEventTriggerEvent.Session.SessionID);
 		if (sessionDetails.Session.SessionType == SessionType.Access) {
-			var ltposh = ExtensionContext.Current.GetSettingValue("PathToLTPoSh");
-			var diag = ExtensionContext.Current.GetSettingValue("PathToDiag");
-			var linuxdiag = ExtensionContext.Current.GetSettingValue("PathToMacLinuxDiag");
-			var server = ExtensionContext.Current.GetSettingValue("AutomateHostname");
+			var ltposh = extensionContext.GetSettingValue("PathToLTPoSh");
+			var diag = extensionContext.GetSettingValue("PathToDiag");
+			var linuxdiag = extensionContext.GetSettingValue("PathToLinuxDiag");
+			var macdiag = extensionContext.GetSettingValue("PathToMacDiag");
+			var server = extensionContext.GetSettingValue("AutomateHostname");
 			var os = sessionDetails.Session.GuestInfo.OperatingSystemName;
-			var timeout = ExtensionContext.Current.GetSettingValue("Timeout");
+			var timeout = extensionContext.GetSettingValue("Timeout");
 			var command = "";
 
-			if ( os.StartsWith("Windows") ) { 
+			// 2023.01.16 -- Joe McCall | Expanded with distinct option for MacOSX and Linux
+			if ( os.Contains("Windows") ) {
 				command = "#!ps\n#maxlength=100000\n#timeout="+ timeout +"\necho 'DIAGNOSTIC-RESPONSE/1'\necho 'DiagnosticType: Automate'\necho 'ContentType: json'\necho ''\n$WarningPreference='SilentlyContinue'; IF([Net.SecurityProtocolType]::Tls) {[Net.ServicePointManager]::SecurityProtocol=[Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls}; IF([Net.SecurityProtocolType]::Tls11) {[Net.ServicePointManager]::SecurityProtocol=[Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls11}; IF([Net.SecurityProtocolType]::Tls12) {[Net.ServicePointManager]::SecurityProtocol=[Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12}; Try {(new-object Net.WebClient).DownloadString('"+ diag +"') | iex; Start-AutomateDiagnostics -ltposh '"+ ltposh +"' -automate_server '"+server+"'} Catch { $_.Exception.Message; Write-Output '!---BEGIN JSON---!'; Write-Output '{\"version\": \"Error loading AutomateDiagnostics\"}' }";
 			}
-			else if ( os.StartsWith("Mac") || os.StartsWith("Linux") ) {
+			else if ( os.Contains("Mac")) {
+				// 2023.01.16 -- Joe McCall | Calling the AutomateDiagnostics.sh sourced from here: https://github.com/noaht8um/CWCAutomateDiagnostics/
+				command = "#!sh\n#maxlength=100000\n#timeout="+ timeout +"\necho 'DIAGNOSTIC-RESPONSE/1'\necho 'DiagnosticType: Automate'\necho 'ContentType: json'\nurl="+macdiag+"; CURL=$(command -v curl); WGET=$(command -v wget); if [ ! -z $CURL ]; then echo $($CURL -s $url | sh); else echo $($WGET -q -O - --no-check-certificate $url | sh); fi";
+			}
+			else if (os.Contains("Linux") ) {
 				command = "#!sh\n#maxlength=100000\n#timeout="+ timeout +"\necho 'DIAGNOSTIC-RESPONSE/1'\necho 'DiagnosticType: Automate'\necho 'ContentType: json'\nurl="+linuxdiag+"; CURL=$(command -v curl); WGET=$(command -v wget); if [ ! -z $CURL ]; then echo $($CURL -s $url | python); else echo $($WGET -q -O - --no-check-certificate $url | python); fi";
 			}
 			else { command = "@echo off\necho No OS Detected, try running the diagnostic again"; }
 			
-			SessionManagerPool.Demux.AddSessionEvent(
+			await SessionManagerPool.Demux.AddSessionEventAsync(
 				sessionEventTriggerEvent.Session.SessionID,
-				new SessionEvent
-				{
-					EventType = SessionEventType.QueuedCommand,
-					Host = "AutomateDiagnostics",
-					Data = command
-				}
+				SessionEventType.QueuedCommand,
+				SessionEventAttributes.NeedsProcessing,
+				"AutomateDiagnostics",
+				command
 			);
 		}
 	}
+
 	private bool IsDiagnosticContent(string eventData) {
 		if (eventData.StartsWith("DIAGNOSTIC-RESPONSE/1") || eventData.StartsWith("\ufeffDIAGNOSTIC-RESPONSE/1")) {
 			return true;
@@ -110,6 +116,7 @@ public class SessionEventTriggerAccessor : IDynamicSessionEventTrigger
 			return false; 
 		}
 	}
+
 	private bool IsRepairResult(string eventData) {
 		if (eventData.Contains("DiagnosticType: ReinstallAutomate") || eventData.Contains("DiagnosticType: RestartAutomate")) {
 			return true;
@@ -118,6 +125,7 @@ public class SessionEventTriggerAccessor : IDynamicSessionEventTrigger
 			return false; 
 		}
 	}
+
 	private bool IsDiagResult(string eventData) {
 		if (eventData.Contains("DiagnosticType: Automate")) {
 			return true;
@@ -126,10 +134,12 @@ public class SessionEventTriggerAccessor : IDynamicSessionEventTrigger
 			return false; 
 		}
 	}
+
     private static string FormatMessage(string message) {
         DateTime now = DateTime.Now;
         return string.Format("{0}: {1}", now.ToString(), message);
     }
+
 	public static void WriteLog(string message) {
         try {
             using (StreamWriter streamWriter = new StreamWriter(string.Concat(Environment.ExpandEnvironmentVariables("%windir%"), "\\temp\\AutomateDiagnostics.log"), true)) {
@@ -138,6 +148,7 @@ public class SessionEventTriggerAccessor : IDynamicSessionEventTrigger
         }
         catch {}
     }
+
 	public static void var_dump(object obj)   
 	{   
 			WriteLog(String.Format("{0,-18} {1}", "Name", "Value"));   
